3e28b44b95b2e83a8497527cdebec82d63a69aa2
ssomasunderam <sanjayansomasunderam@gmail.com>
2023-01-01 07:12:50 -0500

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -2,7 +2,7 @@
 // Used to toggle the menu on small screens when clicking on the menu button
 function myFunction() {
   var x = document.getElementById("navDemo");
-  if (x.className.indexOf("w3-show") == -1) {
+  if (x.className.indexOf("show") == -1) {
     x.className += " show";
   } else { 
     x.className = x.className.replace(" show", "");}}
